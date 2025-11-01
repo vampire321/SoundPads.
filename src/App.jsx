38bf8474047beb36961856f42aaaeb2965704ed5
vampire321 <mh1295551@gmail.com>
2025-11-01 15:35:08 +0530

    shared state

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import padsData from '/pads'
 import Pad from './pad'
 export default function App({darkMode}){
   const [pads, setPads]=React.useState(padsData)
+  function toggle(){
+    console.log("Clicked!")
+  }
 
   const buttonElements=pads.map(pad => (
-    <Pad key={pad.id} color={pad.color} on={pad.on}/>
+    <Pad toggle={toggle} key={pad.id} color={pad.color} on={pad.on}/>
   ))
   return(
     <main>
--- src/pad.jsx
+++ src/pad.jsx
@@ -1,10 +1,6 @@
 import React from "react"
 export default function Pad(props){
     const [on,setOn]=React.useState(props.on)
-
-    function toggle(){
-        setOn(prevOn => !prevOn)
-    }
     return(
         <button style={{backgroundColor: props.color}} 
         className={on ? "on": null}
